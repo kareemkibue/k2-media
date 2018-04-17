35a6a73f5f3c35052c59d447e99dd0f3d3c72826
Kareem Kibue <kareemkibue@gmail.com>
2018-04-17 12:47:00 -0400

    Add module definition file

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * K2 Media types
+ * TypeScript version: 2.8.1
+ */
+
+export const media:any;
+
+export const mediaIos:any;
+
+export function  respond  (minWidth:string, maxWidth:string) :string;
+
+export function respondMin  (minWidth:string) :string;
+export function respondMax  (maxWidth:string) :string;
